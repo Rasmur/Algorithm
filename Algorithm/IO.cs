2d17fa0c90d3fc20ea1b822704e1e92d6fc618c8
Shamil <Shamil@DESKTOP-LJ0TOT2>
2017-06-06 13:49:36 +0500

    Добавил Класс Task и Worker, также в IO парсинг входных данных и затем будет вывод в консоль конечных данных

---
+++ Algorithm/IO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    public class IO
+    {
+        void ParseWorkerAndTask(Worker[] Workers, Task[]Tasks)
+        {
+
+
+
+        }
+
+        void PrintSchedule()
+        {
+
+        }
+
+
+
+    }
+}
---
+++ Algorithm/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    public class Task
+    {
+        string name;
+        bool importance;
+        int deadline;
+        int serialNumber;
+        int duration;
+    }
+}
