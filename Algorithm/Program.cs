c9fc45b7636652811c229f3db200fc5120ea387a
Игорь <igoryan.ru@mail.ru>
2017-06-06 12:42:44 +0500

    Скопировал всё с инета

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1,17 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using btl.generic;
 
-namespace Algorithm
+public class Test
 {
-    class Program
+    //  optimal solution for this is (0.5,0.5)
+    public static double theActualFunction(double[] values)
     {
-        static void Main(string[] args)
-        {
-            //dsdssd
-            //Igor' екудза
-        }//WER
+        if (values.GetLength(0) != 2)
+            throw new ArgumentOutOfRangeException("should only have 2 args");
+
+        double x = values[0];
+        double y = values[1];
+        double n = 9;  //  should be an int, but I don't want to waste time casting.
+
+        double f1 = Math.Pow(15 * x * y * (1 - x) * (1 - y) * Math.Sin(n * Math.PI * x) * Math.Sin(n * Math.PI * y), 2);
+        return f1;
+    }
+
+    public static void Main()
+    {
+        //  Crossover		= 80%
+        //  Mutation		=  5%
+        //  Population size = 100
+        //  Generations		= 2000
+        //  Genome size		= 2
+        GA ga = new GA(0.8, 0.05, 100, 2000, 2);
+
+        ga.FitnessFunction = new GAFunction(theActualFunction);
+
+        //ga.FitnessFile = @"H:\fitness.csv";
+        ga.Elitism = true;
+        ga.Go();
+
+        double[] values;
+        double fitness;
+        ga.GetBest(out values, out fitness);
+        System.Console.WriteLine("Best ({0}):", fitness);
+        for (int i = 0; i < values.Length; i++)
+            System.Console.WriteLine("{0} ", values[i]);
+
+        ga.GetWorst(out values, out fitness);
+        System.Console.WriteLine("\nWorst ({0}):", fitness);
+        for (int i = 0; i < values.Length; i++)
+            System.Console.WriteLine("{0} ", values[i]);
+        Console.ReadLine();
     }
 }
